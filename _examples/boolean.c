a97ae06cb904cb6624fb50a0c36bfc3b9b52d12c
Andrew Steele <andrew@andrewsteele.me.uk>
2015-09-04 20:29:15 +0100

    Added example of Boolean usage

---
+++ _examples/boolean.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    bool run = false;
+
+    if (run == true)
+        printf("Running...\n");
+    else
+        printf("Not running.\n");
+
+    return 0;
+}
